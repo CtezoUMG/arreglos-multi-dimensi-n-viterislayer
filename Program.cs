b5988d658f311cbd56657b51c88bbea8780474bf
viterislayer <torovieri8@gmail.com>
2026-02-23 16:24:42 -0600

    Improve maximum value calculation logic
    Refactor logic to find maximum value in array.

--- Program.cs
+++ Program.cs
@@ -15,11 +15,12 @@ for (int i = 0; i < n; i++)
 }
 
 // --- INICIO DE LÓGICA DEL ALUMNO 
-int maximo = numeros [0]; 
+int maximo = numeros [0];
 for (int i = 1; i < n; i++)
     if (numeros [i] > maximo)
-        maximo = numeros [i]; 
-}
+    {
+        maximo = numeros [i];
+    }
 }
 Console.WriteLine(maximo);
-//FIN DE LOGICA DE EL ALUMNO 
+// ---FIN DE LOGICA DE ALUMNO---
