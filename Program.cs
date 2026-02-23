abc2568844d1090a01b918356a64d32a233e8ea3
viterislayer <torovieri8@gmail.com>
2026-02-23 16:30:05 -0600

    Refactor maximum calculation logic in Program.cs

--- Program.cs
+++ Program.cs
@@ -17,10 +17,10 @@ for (int i = 0; i < n; i++)
 // --- INICIO DE LÓGICA DEL ALUMNO 
 int maximo = numeros [0];
 for (int i = 1; i < n; i++)
-    if (numeros [i] > maximo)
-    {
-        maximo = numeros [i];
-    }
+{
+    if (numeros[i] > maximo)
+        maximo = numeros[i];
+  }
 }
 Console.WriteLine(maximo);
 // ---FIN DE LOGICA DE ALUMNO---
